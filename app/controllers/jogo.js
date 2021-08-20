fcb923e4c402a2f56b7dacde10342c855966a087
Junior Cintra <junior.developer@outlook.com.br>
2021-08-20 02:22:10 -0300

    autenticando parte 2

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,7 @@
 module.exports.jogo = function (app, req, res) {
-  res.render("jogo");
+  if (req.session.autorizado) {
+    res.render("jogo");
+  } else {
+    res.send("Usuario precisa fazer login");
+  }
 };
