fc3fd90ea0488c208b865e0b10d1c31ce082b75f
Junior Cintra <junior.developer@outlook.com.br>
2021-08-20 01:49:53 -0300

    criando controller de autenticacao

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,25 @@
 module.exports.index = function (app, req, res) {
   res.render("index");
 };
+
+module.exports.autenticar = function (app, req, res) {
+  var dadosForm = req.body;
+
+  req.assert("usuario", "Usuário não pode ser vazio").notEmpty();
+  req.assert("senha", "Senha não pode ser vazio").notEmpty();
+
+  var erros = req.validationErrors();
+
+  if (erros) {
+    res.render("cadastro", { validacao: erros, dadosForm: dadosForm });
+    return;
+  }
+
+  var connection = app.config.database;
+
+  var UsuariosDAO = new app.app.models.UsuariosDAO(connection);
+
+  UsuariosDAO.inserirUsuario(dadosForm);
+
+  res.send("podemos cadastrar");
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,8 @@ module.exports = function (app) {
   app.get("/", function (req, res) {
     app.application.controllers.index(app, req, res);
   });
+
+  app.post("/autenticar", function (req, res) {
+    app.application.controllers.autenticar(app, req, res);
+  });
 };
